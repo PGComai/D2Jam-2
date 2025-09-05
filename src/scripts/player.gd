fe8a26fbd212591851279bcd675f71d6e52d66f6
Alex Comai <comai.alex@gmail.com>
2025-09-05 16:59:08 -0700

    switched to state pattern, no coyote or buffer yet

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -1,11 +1,24 @@
 extends CharacterBody2D
 
 
-const SPEED: float = 100.0
-const JUMP: float = 250.0
+const SPEED: float = 125.0
+const JUMP: float = 225.0
 const ACCEL: float = 0.1
+const GRAV: float = 1.5
+const JUMP_HOLD: float = 0.4
+const WALL_FRICTION: float = 0.1
+const HOLD_DIVISOR: float = 3.0
 
 
+enum States {ON_FLOOR,
+			ON_WALL,
+			ASCENDING,
+			FALLING,
+			FLOOR_COYOTE,
+			WALL_COYOTE,}
+
+
+var state: States = States.ON_FLOOR
 var jump_buffer: int = 0
 var wall_jump_effect: float = 0.0:
 	set(value):
@@ -14,95 +27,201 @@ var last_wall_jump_dir: float = 0.0
 var wall_jump_coyote: int = 0
 var wall_normal_coyote: Vector2
 var jump_coyote: int = 0
+var jump_released := false
+
+
+@onready var label: Label = $Label
 
 
 func _ready() -> void:
 	pass
 
 
-func _physics_process(delta: float) -> void:
+func movement_on_floor(delta: float) -> void:
 	var input_x := Input.get_axis("left", "right")
 	
-	if jump_buffer:
-		jump_buffer -= 1
+	velocity.x = input_x * SPEED
+	
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -JUMP
+		jump_released = false
+		#state = States.ASCENDING
+
+
+func movement_on_wall(delta: float) -> void:
+	var input_x := Input.get_axis("left", "right")
+	var wall_normal := get_wall_normal()
+	
+	velocity.x = input_x * SPEED
+	
+	if velocity.y <= 0.0:
+		if Input.is_action_just_released("jump"):
+			jump_released = true
+		
+		if jump_released:
+			velocity += get_gravity() * delta * GRAV
+		else:
+			var hold_factor := 1.0 - pow(-clampf(velocity.y, (-JUMP/HOLD_DIVISOR), 0.0) / (JUMP/HOLD_DIVISOR), 2.0)
+			velocity += get_gravity() * delta * GRAV * lerpf(JUMP_HOLD, 1.0, hold_factor)
+	else:
+		velocity += get_gravity() * delta * GRAV * WALL_FRICTION
+	
+	if Input.is_action_just_pressed("jump"):
+		#velocity.y = minf(velocity.y, 0.0)
+		velocity = Vector2(wall_normal.x, -1.0).normalized() * JUMP
+		wall_jump_effect = 1.0
+		last_wall_jump_dir = wall_normal.x
+		jump_released = false
+		#state = States.ASCENDING
+
+
+func movement_ascending(delta: float) -> void:
+	var input_x := Input.get_axis("left", "right")
+	
+	var target_x_vel: float = lerpf(input_x * SPEED, velocity.x, wall_jump_effect)
+	velocity.x = lerpf(velocity.x, target_x_vel, 0.3)
+	
+	if Input.is_action_just_released("jump"):
+		jump_released = true
+	
+	if jump_released:
+		velocity += get_gravity() * delta * GRAV
+	else:
+		var hold_factor := 1.0 - pow(-clampf(velocity.y, (-JUMP/HOLD_DIVISOR), 0.0) / (JUMP/HOLD_DIVISOR), 2.0)
+		velocity += get_gravity() * delta * GRAV * lerpf(JUMP_HOLD, 1.0, hold_factor)
+
+
+func movement_falling(delta: float) -> void:
+	var input_x := Input.get_axis("left", "right")
+	
+	var target_x_vel: float = lerpf(input_x * SPEED, velocity.x, wall_jump_effect)
+	velocity.x = lerpf(velocity.x, target_x_vel, 0.3)
+	velocity += get_gravity() * delta * GRAV
+
+
+func movement_floor_coyote(delta: float) -> void:
+	var input_x := Input.get_axis("left", "right")
+	
+	velocity.x = input_x * SPEED
+	
+	if Input.is_action_just_pressed("jump"):
+		velocity.y -= JUMP
+		jump_released = false
+		state = States.ASCENDING
+
+
+func movement_wall_coyote(delta: float) -> void:
+	var input_x := Input.get_axis("left", "right")
+
+
+func _physics_process(delta: float) -> void:
+	#var input_x := Input.get_axis("left", "right")
+	#
+	#if jump_buffer:
+		#jump_buffer -= 1
+	#if wall_jump_effect:
+		#wall_jump_effect -= delta * 1.5
+	#if wall_jump_coyote:
+		#wall_jump_coyote -= 1
+	#if jump_coyote:
+		#jump_coyote -= 1
+	#if not is_on_floor():
+		#if is_on_wall():
+			#var wall_normal := get_wall_normal()
+			#wall_normal_coyote = wall_normal
+			#if (signf(input_x) != signf(wall_normal.x) and signf(input_x)) or wall_jump_coyote:
+				#if velocity.y > 0.0:
+					#velocity += get_gravity() * delta * 0.1
+				#else:
+					#velocity += get_gravity() * delta
+				#wall_jump_coyote = 8
+				#if Input.is_action_just_pressed("jump") or jump_buffer > 0:
+					#velocity.y = minf(velocity.y, 0.0)
+					#velocity += Vector2(wall_normal.x, -1.0).normalized() * JUMP
+					#wall_jump_effect = 1.0
+					#last_wall_jump_dir = wall_normal.x
+					#wall_jump_coyote = 0
+					#jump_coyote = 0
+					#jump_buffer = 0
+			#else:
+				#if wall_jump_coyote:
+					#velocity += get_gravity() * delta
+					#if Input.is_action_just_pressed("jump") or jump_buffer > 0:
+						#velocity.y = minf(velocity.y, 0.0)
+						#velocity += Vector2(wall_normal.x, -1.0).normalized() * JUMP
+						#wall_jump_effect = 1.0
+						#last_wall_jump_dir = wall_normal.x
+						#wall_jump_coyote = 0
+						#jump_coyote = 0
+						#jump_buffer = 0
+				#else:
+					#velocity += get_gravity() * delta
+		#else:
+			#if jump_coyote:
+				#if Input.is_action_just_pressed("jump") or jump_buffer > 0:
+					#velocity.y -= JUMP
+					#jump_buffer = 0
+					#jump_coyote = 0
+					#wall_jump_coyote = 0
+			#else:
+				#if wall_jump_coyote:
+					#if Input.is_action_just_pressed("jump") or jump_buffer > 0:
+						#velocity.y = minf(velocity.y, 0.0)
+						#velocity += Vector2(wall_normal_coyote.x, -1.0).normalized() * JUMP
+						#wall_jump_effect = 1.0
+						#last_wall_jump_dir = wall_normal_coyote.x
+						#wall_jump_coyote = 0
+						#jump_coyote = 0
+						#jump_buffer = 0
+				#else:
+					#if Input.is_action_just_pressed("jump"):
+						#jump_buffer = 6
+				#velocity += get_gravity() * delta
+	#else:
+		#wall_jump_coyote = 0
+		#jump_coyote = 3
+		#if Input.is_action_just_pressed("jump") or jump_buffer > 0:
+			#velocity.y -= JUMP
+			#jump_buffer = 0
+			#jump_coyote = 0
+	#
+	#if input_x:
+		#if wall_jump_effect:
+			#velocity.x = lerpf(input_x * SPEED, velocity.x, wall_jump_effect)
+		#else:
+			#if not is_on_floor():
+				#velocity.x = lerpf(velocity.x, input_x * SPEED, 0.2)
+			#else:
+				#velocity.x = input_x * SPEED
+	#else:
+		#if wall_jump_effect:
+			#velocity.x = lerpf(0.0, velocity.x, wall_jump_effect)
+		#else:
+			#velocity.x = 0.0
+	
 	if wall_jump_effect:
 		wall_jump_effect -= delta * 1.5
-	if wall_jump_coyote:
-		wall_jump_coyote -= 1
-	if jump_coyote:
-		jump_coyote -= 1
-	if not is_on_floor():
-		if is_on_wall():
-			var wall_normal := get_wall_normal()
-			wall_normal_coyote = wall_normal
-			if (signf(input_x) != signf(wall_normal.x) and signf(input_x)) or wall_jump_coyote:
-				if velocity.y > 0.0:
-					velocity += get_gravity() * delta * 0.1
-				else:
-					velocity += get_gravity() * delta
-				wall_jump_coyote = 8
-				if Input.is_action_just_pressed("jump") or jump_buffer > 0:
-					velocity.y = minf(velocity.y, 0.0)
-					velocity += Vector2(wall_normal.x, -1.0).normalized() * JUMP
-					wall_jump_effect = 1.0
-					last_wall_jump_dir = wall_normal.x
-					wall_jump_coyote = 0
-					jump_coyote = 0
-					jump_buffer = 0
-			else:
-				if wall_jump_coyote:
-					velocity += get_gravity() * delta
-					if Input.is_action_just_pressed("jump") or jump_buffer > 0:
-						velocity.y = minf(velocity.y, 0.0)
-						velocity += Vector2(wall_normal.x, -1.0).normalized() * JUMP
-						wall_jump_effect = 1.0
-						last_wall_jump_dir = wall_normal.x
-						wall_jump_coyote = 0
-						jump_coyote = 0
-						jump_buffer = 0
-				else:
-					velocity += get_gravity() * delta
-		else:
-			if jump_coyote:
-				if Input.is_action_just_pressed("jump") or jump_buffer > 0:
-					velocity.y -= JUMP
-					jump_buffer = 0
-					jump_coyote = 0
-					wall_jump_coyote = 0
-			else:
-				if wall_jump_coyote:
-					if Input.is_action_just_pressed("jump") or jump_buffer > 0:
-						velocity.y = minf(velocity.y, 0.0)
-						velocity += Vector2(wall_normal_coyote.x, -1.0).normalized() * JUMP
-						wall_jump_effect = 1.0
-						last_wall_jump_dir = wall_normal_coyote.x
-						wall_jump_coyote = 0
-						jump_coyote = 0
-						jump_buffer = 0
-				else:
-					if Input.is_action_just_pressed("jump"):
-						jump_buffer = 6
-				velocity += get_gravity() * delta
-	else:
-		wall_jump_coyote = 0
-		jump_coyote = 3
-		if Input.is_action_just_pressed("jump") or jump_buffer > 0:
-			velocity.y -= JUMP
-			jump_buffer = 0
-			jump_coyote = 0
 	
-	if input_x:
-		if wall_jump_effect:
-			velocity.x = lerpf(input_x * SPEED, velocity.x, wall_jump_effect)
-		else:
-			if not is_on_floor():
-				velocity.x = lerpf(velocity.x, input_x * SPEED, 0.2)
-			else:
-				velocity.x = input_x * SPEED
-	else:
-		if wall_jump_effect:
-			velocity.x = lerpf(0.0, velocity.x, wall_jump_effect)
-		else:
-			velocity.x = 0.0
+	if state == States.ON_FLOOR:
+		label.text = "ON_FLOOR"
+		movement_on_floor(delta)
+	elif state == States.ON_WALL:
+		label.text = "ON_WALL"
+		movement_on_wall(delta)
+	elif state == States.ASCENDING:
+		label.text = "ASCENDING"
+		movement_ascending(delta)
+	elif state == States.FALLING:
+		label.text = "FALLING"
+		movement_falling(delta)
 	
 	move_and_slide()
+	
+	if is_on_floor():
+		state = States.ON_FLOOR
+	elif is_on_wall():
+		state = States.ON_WALL
+	elif velocity.y > 0.0:
+		state = States.FALLING
+	else:
+		state = States.ASCENDING
