ba5f8356a257493212eda0520e1241e93e118072
pgcomai <comai.alex@gmail.com>
2025-09-06 21:35:33 -0700

    visible room numbers

--- src/scripts/camera_tile.gd
+++ src/scripts/camera_tile.gd
@@ -3,24 +3,44 @@ extends Area2D
 class_name CameraTile
 
 
-@export var room: int = 0
+@export var room: int = 0:
+	set(value):
+		room = value
+		if label:
+			label.text = str(room)
 
 
-func _ready() -> void:
-	if get_child_count() == 0:
-		var new_collider := CollisionShape2D.new()
-		var new_shape := RectangleShape2D.new()
-		new_shape.size = Vector2(320.0, 180.0)
-		new_collider.shape = new_shape
-		new_collider.debug_color = Color(0.0, 0.5, 1.0, 0.04)
-		set_collision_layer_value(1, false)
-		set_collision_mask_value(1, false)
-		set_collision_layer_value(4, true)
-		set_collision_mask_value(4, true)
-		add_child(new_collider)
-		add_to_group("camera_tile")
-		body_entered.connect(_on_body_entered)
-		body_exited.connect(_on_body_exited)
+var label: Label
+
+
+func _enter_tree() -> void:
+	for child in get_children():
+		child.queue_free()
+	
+	var new_collider := CollisionShape2D.new()
+	var new_shape := RectangleShape2D.new()
+	new_shape.size = Vector2(320.0, 180.0)
+	new_collider.shape = new_shape
+	new_collider.debug_color = Color(0.0, 0.5, 1.0, 0.04)
+	set_collision_layer_value(1, false)
+	set_collision_mask_value(1, false)
+	set_collision_layer_value(4, true)
+	set_collision_mask_value(4, true)
+	add_child(new_collider)
+	add_to_group("camera_tile")
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+	if Engine.is_editor_hint():
+		var new_label := Label.new()
+		new_label.text = str(room)
+		add_child(new_label)
+		label = new_label
+		var ls := LabelSettings.new()
+		ls.font_color = Color(1.0, 1.0, 1.0, 0.3)
+		ls.outline_size = 2.0
+		ls.outline_color = Color(0.0, 0.0, 0.0, 0.3)
+		label.label_settings = ls
+		label.z_index = 5
 
 
 func _on_body_entered(body) -> void:
