fbd3c831a2339e20178b9c2e24072c7b9593c88a
pgcomai <comai.alex@gmail.com>
2025-09-06 19:07:38 -0700

    attempting rooms

--- src/scripts/camera.gd
+++ src/scripts/camera.gd
@@ -1,13 +1,16 @@
 extends Camera2D
 
 
-@export var player: CharacterBody2D
+@export var player: Player
 
 
 var free_left := false
 var free_right := false
 var free_top := false
 var free_bottom := false
+
+var new_room_center: Vector2
+var goto_new_center := false
 
 
 @onready var sensor_left: Area2D = $SensorLeft
@@ -24,10 +27,49 @@ func _process(delta: float) -> void:
 	free_top = sensor_top.get_overlapping_areas().size()
 	free_bottom = sensor_bottom.get_overlapping_areas().size()
 	
+	var bad_left := false
+	var bad_right := false
+	var bad_top := false
+	var bad_bottom := false
+	
+	if free_left:
+		var o_area: CameraTile = sensor_left.get_overlapping_areas()[0]
+		if player.room == o_area.room:
+			pass
+		else:
+			free_left = false
+			bad_left = true
+	if free_right:
+		var o_area: CameraTile = sensor_right.get_overlapping_areas()[0]
+		if player.room == o_area.room:
+			pass
+		else:
+			free_right = false
+			bad_right = true
+	if free_top:
+		var o_area: CameraTile = sensor_top.get_overlapping_areas()[0]
+		if player.room == o_area.room:
+			pass
+		else:
+			free_top = false
+			bad_top = true
+	if free_bottom:
+		var o_area: CameraTile = sensor_bottom.get_overlapping_areas()[0]
+		if player.room == o_area.room:
+			pass
+		else:
+			free_bottom = false
+			bad_bottom = true
+	
 	if player:
 		target_pos = player.global_position - Vector2(0.0, 0.5)
 	
-	if target_pos:
+	if goto_new_center:
+		global_position = global_position.lerp(new_room_center, 0.3)
+		if global_position.is_equal_approx(new_room_center):
+			global_position = new_room_center
+			goto_new_center = false
+	elif target_pos:
 		var target_dir := global_position.direction_to(target_pos)
 		var next_pos := global_position.lerp(target_pos, 0.2)
 		
@@ -45,3 +87,8 @@ func _process(delta: float) -> void:
 				next_pos.y = global_position.y
 		
 		global_position = next_pos
+
+
+func _on_player_room_changed() -> void:
+	new_room_center = player.room_center
+	goto_new_center = true
---
+++ src/scripts/ghost_get.gd
@@ -0,0 +1,28 @@
+@tool
+extends Area2D
+class_name GhostGet
+
+
+const GHOST_SPRITE = preload("res://scenes/ghost_sprite.tscn")
+
+
+func _ready() -> void:
+	if get_child_count() == 0:
+		var new_collider := CollisionShape2D.new()
+		var new_shape := RectangleShape2D.new()
+		new_shape.size = Vector2(8.0, 16.0)
+		new_collider.shape = new_shape
+		new_collider.debug_color = Color(0.0, 1.0, 0.5, 0.1)
+		add_child(new_collider)
+		add_to_group("ghost_get")
+		var new_sprite = GHOST_SPRITE.instantiate()
+		new_sprite.modulate = Color.BLUE
+		add_child(new_sprite)
+		
+		body_entered.connect(_on_body_entered)
+
+
+func _on_body_entered(body) -> void:
+	if body.is_in_group("player"):
+		var player: Player = body
+		player.call_deferred("add_ghost")
